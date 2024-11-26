b991b5db403ae5510cc1f54e3d0a2502345d559a
Max <mshevchenko1984@gmail.com>
2024-11-26 13:21:34 +0200

    Initial commit

---
+++ src/app/FileHandler.java
@@ -0,0 +1,41 @@
+package app;
+
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileHandler {
+
+    public String createFile(String path) {
+        Path newFile = Paths.get(path);
+
+        try {
+            newFile = Files.createFile(newFile);
+        } catch (FileAlreadyExistsException e) {
+            return "File already exists!";
+        } catch (IOException e) {
+            return "Something wrong " + e.getMessage();
+        }
+        return "Created " + newFile;
+    }
+
+    public String writeToFile(Path path, String content) {
+        try {
+            Files.writeString(path, content);
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+        return "Recorded in " + path;
+    }
+
+    public String readFromFile(String path) {
+        try {
+            return Files.readString(Paths.get(path));
+        } catch (IOException e) {
+            return "Something wrong " + e.getMessage();
+        }
+    }
+}
